6af463695d758b91ceba4daced81ba7e58a09276
sujitas400 <40168604+sujitas400@users.noreply.github.com>
2020-01-26 16:40:57 +0530

    commented chrome driver

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -29,8 +29,8 @@ public class Hook extends BaseUtil{
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "C:\libs\chromedriver.exe");
-        base.Driver = new ChromeDriver();
+       // System.setProperty("webdriver.chrome.driver", "C:\libs\chromedriver.exe");
+       // base.Driver = new ChromeDriver();
     }
 
 
